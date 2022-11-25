bc3c360bca67e0e488c91ea19a07a579f0892fd2
israel.bibiano <israel.bibiano@minu.mx>
2022-11-25 15:51:38 -0600

    Se agregan más casos de prueba de práctica

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.opencart.com/',
+    baseUrl: 'https://vamonos-finance.herokuapp.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,8 +1,25 @@
-describe('Este será mi set de pruebas', () => {
-    it('CP01 - Este es mi primera prueba', () => {
-        expect(true).to.equal(true)
-    });
-    it('CP02 - Esta es mi segunda prueba',()=> {
-        expect(false).to.equal(false)
-    });
+describe("Este será mi set de pruebas", () => {
+  it("CP01 - Este es mi primera prueba", () => {
+    expect(true).to.equal(true);
+  });
+  it("CP02 - Esta es mi segunda prueba", () => {
+    expect(false).to.equal(false);
+  });
+  it("Localizando elementos", () => {
+    cy.visit("/");
+    cy.get(".nav-link")
+      .contains("Register")
+      .click()
+      .get('[placeholder="First Name"]')
+      .type("Prueba")
+      .get('[name="birthdate"]')
+      .type("2001-07-21");
+  });
+  it.only("Login",()=>{
+    cy.visit('/')
+        .get('[name="username"]').type('jmendez')
+        .get('[name="password"]').type('12345678')
+        .get('.btn').contains("Log In").should('be.visible').click()
+        .get('.alert').should('contain','Welcome')
+  })
 });

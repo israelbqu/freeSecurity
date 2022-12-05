7f861455048a56f894362431668384bc0b2150f7
israel.bibiano <israel.bibiano@minu.mx>
2022-12-05 10:44:36 -0600

    Se carga proyecto final del FreeSecurity para su consulta

---
+++ cypress/e2e/freeSecurity.cy.js
@@ -0,0 +1,14 @@
+var username = "jmendez"
+
+describe('Pruebas Free Security', () => {
+    it.only('CP02 - Israel', () => {
+        cy.viewport(390, 8440)
+      cy.visit("/")
+      .get("#login2").click()
+      .get("#loginusername").type("jmendez")
+      .get("#loginpassword").type("123456")
+      .get("#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary").click()
+      .get("#nameofuser")
+        .should("contain", "Welcome " + username)
+    });
+  });
---
+++ cypress/e2e/freeSecurity2.cy.js
@@ -0,0 +1,50 @@
+/*
+buttonLogin: #login2
+userName: #loginusername
+Password: #loginpassword
+buttonLogin2: [onclick="logIn()"]
+
+describe - Descripción del set de pruebas, "simulando" que son los ESCENARIOS de Prueba 
+
+1. Ingresar a la liga (www.)
+2. Dar clic en el enlace de Registro
+3. Registrar Datos Personales
+4. Dar clic en Registrarse
+5. Visualizar página "Home" 
+*/
+
+var username = "jmendez"
+describe('Free Security 2do día', () => {
+    //it - Casos de prueba a ejecutar
+    it('CP02 - Login - Jason', () => {
+        cy.visit("/")
+        .get("#login2")
+        .get("#loginusername").type(username)
+        .get("#loginpassword").type("123456")
+        .get("[onclick='logIn()']").click()
+    });
+
+    it('CP02 - Login - Sixto', () => {
+        cy.visit("/")
+        .get("#login2")
+        .get("#loginusername").type("jmendez")
+        .get("#loginpassword").type("123456")
+        .get("[onclick='logIn()']").click()
+    });
+    it('CP02 - Login - Abdiel', () => {
+        cy.visit("/")
+        .get("#login2")
+        .get("#loginusername").type("jmendez")
+        .get("#loginpassword").type("123456")
+        .get("[onclick='logIn()']").click()
+    });
+    it.only('CP02 - Login - Israel', () => {
+        cy.visit("/")
+        .get("#login2").click()
+        .get("#loginusername").type(username)
+        .get("#loginpassword").type("123456")
+        .get("[onclick='logIn()']").click()
+        //validar home
+        .get("#nameofuser").should('contain','Welcome ' + username)
+    });
+});

0b9040ff8e3eba687a07ca4f13ead1fea2ca3cc3
israel.bibiano <israel.bibiano@minu.mx>
2022-12-05 10:55:01 -0600

    Se carga proyecto final del FreeSecurity para su consulta

--- cypress/e2e/freeSecurity.cy.js
+++ cypress/e2e/freeSecurity.cy.js
@@ -10,16 +10,5 @@ describe('Pruebas Free Security', () => {
       .get("#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary").click()
       .get("#nameofuser")
         .should("contain", "Welcome " + username)
-<<<<<<< HEAD
-=======
-
-      
-
-
-//      .get("#user-name")
-
-//jmendez - 123456
-
->>>>>>> 475b1658201bff91a798bdc862ffe94aec98171d
     });
   });
